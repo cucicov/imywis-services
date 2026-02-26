62a3e223539eed51fbaf92ed32ce847bbe7361b5
cucicov <cucicov@gmail.com>
2026-02-26 21:54:33 +0200

    post-init.

---
+++ src/main/java/com/example/imywisservices/ImywisServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.imywisservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImywisServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImywisServicesApplication.class, args);
+    }
+
+}
