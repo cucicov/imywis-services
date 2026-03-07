f624bfe79f33f8799d8f9becfd2b78283c945e0c
cucicov <cucicov@gmail.com>
2026-03-07 20:19:24 +0200

    add cors for vercel.

--- src/main/java/com/example/imywisservices/config/SecurityConfig.java
+++ src/main/java/com/example/imywisservices/config/SecurityConfig.java
@@ -33,7 +33,10 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://imywis.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
